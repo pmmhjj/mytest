3c2f69d51305bb48717c9232af87b223dcb40212
pmmhjj <peimingming2016@gmail.com>
2016-11-01 22:16:13 +0800

    add the sleep condition

--- src/mytest/MyHelloWorld.java
+++ src/mytest/MyHelloWorld.java
@@ -3,5 +3,12 @@ package mytest;
 public class MyHelloWorld {
   public static void main(String args[]){
     System.out.println("hello world java!");
+    
+    try {
+      Thread.sleep(10000);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
   }
 }
